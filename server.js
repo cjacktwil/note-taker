29b8e0f09a5eb3d122b0469fdf00f7b19b54d76c
Tracey Jackson <62776009+cjacktwil@users.noreply.github.com>
2020-08-12 12:01:43 -0500

    working get route and heroku launched on heroku

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const { notes } = require('./db/db.json');
+
+const app = express();
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.listen(3003, () => {
+    console.log(`API server now on port 3003`);
+});
