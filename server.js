f7af42288e24daf23d420202609951d83300c50d
Tracey Jackson <62776009+cjacktwil@users.noreply.github.com>
2020-08-12 12:09:58 -0500

    updated port

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const { notes } = require('./db/db.json');
 
+const PORT = process.env.PORT || 3003;
+
 const app = express();
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
-app.listen(3003, () => {
-    console.log(`API server now on port 3003`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
