d12b8f4fc4769814834d76aaeb6af678ddc7f7c6
Tracey Jackson <62776009+cjacktwil@users.noreply.github.com>
2020-08-12 15:14:47 -0500

    completed post and added validation

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const { notes } = require('./db/db.json');
 
@@ -6,8 +8,44 @@ const PORT = process.env.PORT || 3003;
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.json());
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray}, null, 2)
+    );
+    return note;
+};
+
+function validateNote(note) {
+    if (!note.name || typeof note.name !== 'string') {
+        return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+        return false;
+    }
+    return true;
+};
+
 app.get('/api/notes', (req, res) => {
     res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    if (!validateNote(req.body)) {
+        res.status(400).send('Please be sure that the note has a title and some text.');
+    } else {
+    const note = createNewNote(req.body, notes);
+
+    res.json(req.body);
+    }
 });
 
 app.listen(PORT, () => {
